56f5c3cd591ad5a78628edb6d265bbb329b2413b
p4paya <80629228+kt-tran@users.noreply.github.com>
2022-11-09 23:17:49 +1000

    Update ledSimple.py

--- ledSimple.py
+++ ledSimple.py
@@ -19,6 +19,49 @@ try:
     while True:
         # if ("Saturday" == x.strftime("%A")):
         if (binWeek == 1):  # normal bin only
+#!/usr/bin/python
+
+
+# setting up GPIO
+DAY = 86400  # number of seconds in a day
+x = datetime.datetime.now()
+CurrentWeek = x.isocalendar()[1]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16, GPIO.OUT)  # green light
+GPIO.setup(26, GPIO.OUT)  # yellow light
+GPIO.output(16, 0)
+GPIO.output(26, 0)
+
+
+try:
+    while True:
+        # if ("Saturday" == x.strftime("%A")):
+        if (CurrentWeek % 2 == 0):  # recycle & normal bin when the week of the year is odd
+            GPIO.output(26, True)
+            GPIO.output(16, True)
+            time.sleep(30)
+
+        else:  # normal bin only when the week of the year is even
+            GPIO.output(16, True)
+            time.sleep(30)
+
+        GPIO.output(26, False)
+        GPIO.output(16, False)
+
+
+except KeyboardInterrupt:
+    # here you put any code you want to run before the program
+    # exits when you press CTRL+C
+    print("\nYou killed me :(")
+
+except:
+    # this catches ALL other exceptions including errors.
+    # You won't get any error messages for debugging
+    # so only use it once your code is working
+    print("Other error or exception occurred!")
+
+finally:
+    GPIO.cleanup()  # this ensures a clean exit
             GPIO.output(16, True)
             time.sleep(30)
             binWeek = 2
